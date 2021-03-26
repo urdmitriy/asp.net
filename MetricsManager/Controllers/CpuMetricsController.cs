a254b4aef9259dcebbd26f8c01429d7aa4c2bb4d
Uryupin <urdmitriy@yandex.ru>
2021-03-26 15:36:13 +0300

    Добавление обработки кластеров

--- MetricsManager/Controllers/CpuMetricsController.cs
+++ MetricsManager/Controllers/CpuMetricsController.cs
@@ -14,13 +14,25 @@ namespace MetricsManager.Controllers
         [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}/percentiles/{percentile}")]
         public IActionResult GetMetricsFromAgentByPercentille([FromRoute] int agentId, [FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime, [FromRoute] Percentile percentile)
         {
-            return Ok("GetMetricsCpuByPercentille");
+            return Ok("");
         }
 
         [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}")]
         public IActionResult GetMetricsFromAgent([FromRoute] int agentId, [FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
         {
-            return Ok("GetMetricsCpu");
+            return Ok("");
+        }
+
+        [HttpGet("cluster/from/{fromTime}/to/{toTime}")]
+        public IActionResult GetMetricsFromAllCluster([FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+        {
+            return Ok();
+        }
+
+        [HttpGet("cluster/from/{fromTime}/to/{toTime}/percentiles/{percentile}")]
+        public IActionResult GetMetricsByPercentileFromAllCluster([FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime, [FromRoute] Percentile percentile)
+        {
+            return Ok();
         }
     }
 }
